02ec868571d8d323ace9b58c0db5cd320bfb5cb9
fjoeda <f.yudanto@gmail.com>
2018-03-23 23:09:34 +0700

    Route done. Code on MapTest Package

--- src/MapTest/Controller.java
+++ src/MapTest/Controller.java
@@ -1,16 +1,21 @@
 package MapTest;
 
 import SerialComm.SerialCommunication;
+import TextParser.StringParser;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
 import com.lynden.gmapsfx.javascript.event.GMapMouseEvent;
 import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.*;
+import com.lynden.gmapsfx.shapes.Polyline;
+import com.lynden.gmapsfx.shapes.PolylineOptions;
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import javax.swing.text.Position;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +33,15 @@ public class Controller implements Initializable, MapComponentInitializedListene
     private LatLong position;
     private ArrayList<LatLong> positionList = new ArrayList<>();
     private SerialCommunication serial;
-    
+    private AnimationTimer timer;
+
+    long lastTimerCall;
+    LatLong positionLast;
+    LatLong positionNow;
+
+    MVCArray mvc;
+    PolylineOptions polylineOptions;
+    ArrayList<LatLong> routes = new ArrayList<>();
 
     public void buttonClick(ActionEvent actionEvent) {
     }
@@ -51,8 +64,23 @@ public class Controller implements Initializable, MapComponentInitializedListene
         map.addMouseEventHandler(UIEventType.click,(GMapMouseEvent event)->{
             position = event.getLatLong();
             map.addMarker((new Marker((new MarkerOptions()).position(position))));
+            if(!positionList.contains(position)) {
+                positionList.add(position);
+
+            }
+
 
         });
+
+        positionLast = new LatLong(1.32,0.23);
+
+        mvc = new MVCArray(routes.toArray());
+        polylineOptions = new PolylineOptions()
+                .path(mvc)
+                .strokeColor("blue")
+                .strokeWeight(2);
+        Polyline poly = new Polyline(polylineOptions);
+        map.addMapShape(poly);
 
 
 
@@ -64,17 +92,52 @@ public class Controller implements Initializable, MapComponentInitializedListene
         
         mapView.addMapInializedListener(this);
         serial = new SerialCommunication();
-        serial.setPortName("COM4");
+        serial.setPortName("COM14");
         serial.setBaudRate(9600);
+        lastTimerCall = System.nanoTime();
+
+
+
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if(now > lastTimerCall + 1_000_000_000L && serial.getReceivedMessage()!=null){
+
+                    try{
+                        positionNow = new LatLong(StringParser.getLatitude(serial.getReceivedMessage()),
+                                StringParser.getLongitude(serial.getReceivedMessage()));
+                        System.out.println(positionNow);
+                        if((positionNow.getLatitude()!= positionLast.getLatitude())||(positionNow.getLongitude()!=positionLast.getLongitude())){
+                            routes.add(positionNow);
+                            positionLast = positionNow;
+                            map.setCenter(positionNow);
+                            mvc.push(positionNow);
+                            System.out.println(serial.getReceivedMessage());
+
+
+                        }
+
+
+                    }catch (Exception e){
+
+                    }
+                    lastTimerCall=now;
+
+
+                }
+            }
+        };
 
     }
 
     public void clearWaypoint(ActionEvent actionEvent) {
+        map.clearMarkers();
     }
 
     public void connectSerial(ActionEvent actionEvent) {
         try {
             serial.connectToSerial();
+            timer.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
