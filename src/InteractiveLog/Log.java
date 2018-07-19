d1b5f3e0d805d1f29251bcf9688bf58e1f40a26a
Aldian Putra <aldianputra99@gmail.com>
2018-07-20 01:12:01 +0700

    UI buat Log. Masih polosan karena di laptopku ngga bisa jalan programnya mas

---
+++ src/InteractiveLog/Log.java
@@ -0,0 +1,21 @@
+package InteractiveLog;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Log extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("InteractiveLog.fxml"));
+        primaryStage.setTitle("Log");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
