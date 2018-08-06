496464bc30a9fc73a39cbb0aa9cbe8eb7c000382
aikbbnaur <aikbbnaur@gmail.com>
2018-08-06 13:59:57 +0700

    3D visualization ok. Cek kesesuaian rotasi dengan wahana

--- src/InteractiveLog/Controller.java
+++ src/InteractiveLog/Controller.java
@@ -1,13 +1,33 @@
 package InteractiveLog;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML
+    private Button btnLoad;
+    private Window primaryStage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void LoadFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Pilih File Waypoint");
+        fileChooser.setInitialDirectory(new File("D:\\IdeaProjects\\AvionicIndicator\\waypoint"));
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("CVS Files", "*.csv")
+        );
+        fileChooser.showOpenDialog(primaryStage);
     }
 }
--- src/InteractiveLog/Log.java
+++ src/InteractiveLog/Log.java
@@ -4,18 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Log extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
+    sample.Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("InteractiveLog.fxml"));
+        //Controller controller = FXMLLoader.load(getClass().getResourceAsStream())
+        //controller = FXMLLoader.load(getClass().getResource("InteractiveLog.fxml"))
+        controller  = (new FXMLLoader(getClass().getResource("InteractiveLog.fxml"))).getController();
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Pilih File Waypoint");
+
         primaryStage.setTitle("Log");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        //fileChooser.showOpenDialog(primaryStage);
     }
 }
