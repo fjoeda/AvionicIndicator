9454d704eaacf413991f433d2f819aae68621f3a
fjoeda <f.yudanto@gmail.com>
2018-08-31 21:45:12 +0700

    Update map + plane icon

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,34 +6,24 @@ import TextParser.StringParser;
 import Visualize3D.Importer3D;
 import Visualize3D.SubSceneContainer;
 import Visualize3D.ViewerModel;
-import com.lynden.gmapsfx.GoogleMapView;
-import com.lynden.gmapsfx.MapComponentInitializedListener;
-import com.lynden.gmapsfx.javascript.event.GMapMouseEvent;
-import com.lynden.gmapsfx.javascript.event.UIEventType;
-import com.lynden.gmapsfx.javascript.object.*;
-import com.lynden.gmapsfx.shapes.Polyline;
-import com.lynden.gmapsfx.shapes.PolylineOptions;
+import com.sothawo.mapjfx.*;
+import com.sothawo.mapjfx.event.MapViewEvent;
 import gnu.io.*;
 import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import org.controlsfx.control.ToggleSwitch;
 
@@ -55,8 +45,7 @@ import java.text.NumberFormat;
 import java.util.*;
 import java.util.List;
 
-public class Controller implements Initializable, MapComponentInitializedListener {
-    private static final Random RND = new Random();
+public class Controller implements Initializable {
     public Label Status;
     public Label BatteryLabel;
     public static HBox TitleBar;
@@ -82,7 +71,7 @@ public class Controller implements Initializable, MapComponentInitializedListene
     public ComboBox<String> PortList;
     public ComboBox<String> BaudList;
     public ToggleSwitch ConnectButton;
-    public GoogleMapView mapView;
+    public MapView mapView;
     public GridPane SecAvionic;
     public Pane Pane3D;
     public SubSceneContainer subSceneContainer;
@@ -90,16 +79,16 @@ public class Controller implements Initializable, MapComponentInitializedListene
     private String PortName = null;
     private String BaudRate = null;
     private ViewerModel viewer;
-    private GoogleMap map;
-    private LatLong position;
-    private ArrayList<LatLong> positionList = new ArrayList<>();
-    private LatLong positionLast;
-    private LatLong positionNow;
-    private MVCArray mvc;
-    private PolylineOptions polylineOptions;
-    private ArrayList<LatLong> routes = new ArrayList<>();
+    private Coordinate position;
+    private ArrayList<Coordinate> positionList = new ArrayList<>();
+    private ArrayList<Marker> markerList = new ArrayList<>();
+    private Coordinate positionLast;
+    private Coordinate positionNow;
+    private ArrayList<Coordinate> routes = new ArrayList<>();
     private boolean isConnectedToSerial = false;
     private ArrayList<Waypoint> waypoints = new ArrayList<>();
+    private Marker planeMarker;
+    private CoordinateLine coordinateLine;
 
     private int index = 0;
     private int loadIndex = 0;
@@ -119,10 +108,10 @@ public class Controller implements Initializable, MapComponentInitializedListene
     private BufferedReader fileReader = null;
 
     private long           lastTimerCall;
-    private long           lastSpeedDataGot;
+    private long           lastRouteDataGot;
     private double         speed;
     private AnimationTimer timer;
-    private AnimationTimer speedometerTimer;
+    private AnimationTimer routeTimer;
 
     SerialCommunication serial = null;
 
@@ -133,6 +122,7 @@ public class Controller implements Initializable, MapComponentInitializedListene
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        String BING_MAP_API_KEY = "AjBEkbJVIF_enJ7KdZTSXxNgn58ADVVRqFNKbSBeSCmNukw4hQYAAcaIM61q2mGp";
         compass   = new AirCompass();
         horizon   = new Horizon();
         altimeter = new Altimeter();
@@ -143,9 +133,9 @@ public class Controller implements Initializable, MapComponentInitializedListene
                 .animated(false)
                 .maxValue(30)
                 .majorTickSpace(2)
+                .value(0)
                 .build();
 
-        mapView.addMapInializedListener(this);
         SecAvionic.add(horizon,0,0);
         SecAvionic.add(compass,0,2);
         SecAvionic.add(speedometer,1,0);
@@ -161,21 +151,58 @@ public class Controller implements Initializable, MapComponentInitializedListene
         initialize3DScene();
 
         lastTimerCall = System.nanoTime();
-        lastSpeedDataGot = System.nanoTime();
+        lastRouteDataGot = System.nanoTime();
+
+        //Initialize Map
+        mapView.setBingMapsApiKey(BING_MAP_API_KEY);
+        mapView.setMapType(MapType.BINGMAPS_ROAD);
+        mapView.setZoom(12);
+        mapView.setCenter(new Coordinate(-7.7713847,110.3774998));
+        planeMarker = new Marker(getClass().getResource("Object/PlaneIcon.png"))
+                .setPosition(new Coordinate(-7.7713847,110.3774998))
+                .setVisible(true);
+
+
+        //Map Event Handler
+        mapView.addEventHandler(MapViewEvent.MAP_CLICKED, event -> {
+            position = event.getCoordinate();
+            Marker marker = new Marker(getClass().getResource("Object/marker1.png")).setPosition(position).setVisible(true);
+            mapView.addMarker(marker);
+            markerList.add(marker);
+            System.out.println(event.getCoordinate());
+            if(!positionList.contains(position)){
+                positionList.add(position);
+                waypoints.add(new Waypoint(index,Double.toString(position.getLatitude()),Double.toString(position.getLongitude())));
+                index++;
+            }
+
+        });
+
+        mapView.initialize();
+
 
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 if(now > lastTimerCall + 100_000_000L && serial.getReceivedMessage()!=null){
                     System.out.println(serial.getReceivedMessage());
-                    refreshMapRoute();
                     refreshOrientation();
                     System.out.println(StringParser.getDataLength(serial.getReceivedMessage()));
                     lastTimerCall = now;
                 }
             }
         };
-        speedometer.setValue(0);
+
+
+        routeTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if(now>lastRouteDataGot + 1_000_000_000L && serial.getReceivedMessage()!=null){
+                    refreshMapRoute();
+                    lastRouteDataGot = now;
+                }
+            }
+        };
 
         /*
         * Initialize toggle button handlers
@@ -188,24 +215,26 @@ public class Controller implements Initializable, MapComponentInitializedListene
                            serial = new SerialCommunication(PortName,Integer.valueOf(BaudRate));
                            serial.connectToSerial();
                            isConnectedToSerial = true;
+                           routes.add(positionNow);
+                           mapView.addMarker(planeMarker);
                            timer.start();
-                           speedometerTimer.start();
+                           routeTimer.start();
 
                        } else {
                            new Alert(Alert.AlertType.ERROR,"You haven't select any COM port or baud rate or both").showAndWait();
                            ConnectButton.setSelected(false);
                        }
                    }catch (Exception e){
-
+                        System.out.println(e.getMessage());
                    }
 
                }
-           } else if (!newValue){
+           } else {
                if(isConnectedToSerial){
                    serial.disconnectSerial();
                    isConnectedToSerial = false;
                    timer.stop();
-                   speedometerTimer.stop();
+                   routeTimer.stop();
                }
            }
         });
@@ -238,56 +267,8 @@ public class Controller implements Initializable, MapComponentInitializedListene
 
 
 
-    @Override
-    public void mapInitialized() {
-        //LatLong MyPosition = new LatLong(-7.8891243,113.71372344);
-        LatLong MyPosition = new LatLong(-7.7692766,110.3736047);
-        MapOptions mapOptions = new MapOptions();
 
-        mapOptions.center(MyPosition).mapType(MapTypeIdEnum.SATELLITE)
-                .zoom(12)
-                .mapTypeControl(false)
-                .streetViewControl(false)
-                .rotateControl(false)
-                .scaleControl(false);
 
-        map = mapView.createMap(mapOptions);
-        System.out.println(mapView.isCache());
-        mapView.setCache(true);
-        System.out.println(mapView.getCacheHint());
-        map.addMouseEventHandler(UIEventType.click,(GMapMouseEvent event)->{
-            position = event.getLatLong();
-            map.addMarker((new Marker((new MarkerOptions()).position(position))));
-            if(!positionList.contains(position)) {
-                positionList.add(position);
-
-                Waypoint waypoint = new Waypoint(index, Double.toString(position.getLatitude()), Double.toString(position.getLongitude()));
-                waypoints.add(waypoint);
-
-                index++;
-            }
-        });
-
-        positionLast = new LatLong(1.32,0.23);
-
-        mvc = new MVCArray(routes.toArray());
-        polylineOptions = new PolylineOptions()
-                .path(mvc)
-                .strokeColor("blue")
-                .strokeWeight(2);
-        Polyline poly = new Polyline(polylineOptions);
-        map.addMapShape(poly);
-        speedometerTimer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                if(now > lastTimerCall + 100_000_000L && serial.getReceivedMessage()!=null){
-                    speed = StringParser.getAirspeed(serial.getReceivedMessage());
-                    speedometer.setValue(speed);
-                    lastSpeedDataGot = now;
-                }
-            }
-        };
-    }
 
     public void RefreshPortList(MouseEvent mouseEvent) {
         Enumeration<CommPortIdentifier> portList = CommPortIdentifier.getPortIdentifiers();
@@ -329,22 +310,32 @@ public class Controller implements Initializable, MapComponentInitializedListene
     }
 
     private void refreshMapRoute(){
-        positionNow = new LatLong(StringParser.getLatitude(serial.getReceivedMessage()),
+        positionNow = new Coordinate(StringParser.getLatitude(serial.getReceivedMessage()),
                 StringParser.getLongitude(serial.getReceivedMessage()));
 
-        map.setCenter(positionNow);
+        //mapView.setCenter(positionNow);
+        mapView.removeMarker(planeMarker);
+        planeMarker.setPosition(positionNow).setVisible(true);
+        mapView.addMarker(planeMarker);
+
         try{
 
             System.out.println(positionNow);
             if((positionNow.getLatitude()!= positionLast.getLatitude())||(positionNow.getLongitude()!=positionLast.getLongitude())){
                 routes.add(positionNow);
+                mapView.removeCoordinateLine(coordinateLine);
+                coordinateLine = new CoordinateLine(routes);
+                mapView.addCoordinateLine(coordinateLine);
+
                 positionLast = positionNow;
-                map.setCenter(positionNow);
-                map.setZoom(16);
-                mvc.push(positionNow);
+
+                mapView.setZoom(16);
+                coordinateLine.setColor(Color.YELLOWGREEN)
+                        .setVisible(true)
+                        .setWidth(7);
             }
         } catch (Exception e){
-
+            System.out.println(e.getMessage());
         }
     }
 
@@ -364,7 +355,7 @@ public class Controller implements Initializable, MapComponentInitializedListene
                     Status.setText("DISARMED");
                 }
             } catch (Exception e) {
-
+                System.out.println(e.getMessage());
             }
         }
 
@@ -405,7 +396,6 @@ public class Controller implements Initializable, MapComponentInitializedListene
     }
 
     public void loadWaypoint(ActionEvent actionEvent) {
-        map.clearMarkers();
 
         FileChooser fileChooser = new FileChooser();
         //fileChooser.showOpenDialog();
@@ -419,8 +409,8 @@ public class Controller implements Initializable, MapComponentInitializedListene
                 String[] tokens = line.split(COMMA_DELIMITER);
 
                 if (tokens.length > 0) {
-                    LatLong pos = new LatLong(Double.parseDouble(tokens[1]),Double.parseDouble(tokens[2]));
-                    map.addMarker((new Marker((new MarkerOptions()).position(pos))));
+                    Coordinate pos = new Coordinate(Double.parseDouble(tokens[1]),Double.parseDouble(tokens[2]));
+                    ///Tambah marker
                     Waypoint waypoint = new Waypoint(Integer.valueOf(tokens[0]), Double.toString(pos.getLatitude()), Double.toString(pos.getLongitude()));
                     waypoints.add(waypoint);
                     System.out.println(String.valueOf(waypoint.getId()) + ", " + String.valueOf(waypoint.getLatitude()) + ", " + String.valueOf(waypoint.getLongitude()));
@@ -446,7 +436,10 @@ public class Controller implements Initializable, MapComponentInitializedListene
     }
 
     public void clearWaypoint(ActionEvent actionEvent) {
-        map.clearMarkers();
+        for(Marker marker : markerList){
+            mapView.removeMarker(marker);
+        }
+        waypoints.clear();
         positionList.clear();
         index = 0;
     }
